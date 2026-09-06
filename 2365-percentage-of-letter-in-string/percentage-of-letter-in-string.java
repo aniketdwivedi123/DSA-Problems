5399ff735e9f6efd9e8c87e6b1b365456d9f60f5
Aniket Dwivedi <aniketdwivedi1422@gmail.com>
2026-09-06 23:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (74.91%) - LeetSync

---
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+        int freq = 0;
+        for (char c : s.toCharArray()){
+            if(c== letter) freq++;
+        }
+        return freq * 100/s.length();
+        
+    }
+}
